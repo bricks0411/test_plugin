422211d5b7ef8c1785e0ca02e97837ec3e369d25
Bricks0411 <1072391442@qq.com>
2025-12-19 19:06:34 +0800

    update: sub

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ class RussianRoulette(Star):
 
     @filter.command("sub")
     async def GetMinus(self, event: AstrMessageEvent, a: int, b: int):
-        yield event.piain_result(f"结果是：{a + b}！")
+        yield event.plain_result(f"结果是：{a - b}！")
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
