6d23dc6d94e8b6e8f36f7df1a3391ffde235983f
Bricks0411 <1072391442@qq.com>
2025-12-20 14:59:20 +0800

    fixed

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ class RussianRoulette(Star):
         yield event.plain_result(f"结果是：{a - b}！")
 
     @filter.message()
-    async def Fake_say(self, event: AstrMessageEvent, *args):
+    async def Fake_say(self, event: AstrMessageEvent):
         messages = event.get_messages()
 
         logger.debug(
