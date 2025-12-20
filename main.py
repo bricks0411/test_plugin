6544e84976847aec1ea443cf86d3639ffd90625e
Bricks0411 <1072391442@qq.com>
2025-12-20 14:32:27 +0800

    修复伪造bug

--- main.py
+++ main.py
@@ -78,8 +78,8 @@ class RussianRoulette(Star):
 
         logger.info(
             f"[fake_say] 触发伪造发言 | "
-            f"target_uin={at.qq} | "
-            f"target_name={at.name} | "
+            f"target_uin={at_target.qq} | "
+            f"target_name={at_target.name} | "
             f"content={content}"
         )
         yield event.chain_result([node])
